2965ff5ca3853d000802124c45b9e4ebbf05c7cc
edurevsky <edurevsky@gmail.com>
2022-08-11 14:03:15 -0300

    add contact repository

---
+++ src/main/java/dev/edurevsky/contacts/repositories/ContactRepository.java
@@ -0,0 +1,9 @@
+package dev.edurevsky.contacts.repositories;
+
+import dev.edurevsky.contacts.models.Contact;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ContactRepository extends JpaRepository<Contact, Long> {
+}
