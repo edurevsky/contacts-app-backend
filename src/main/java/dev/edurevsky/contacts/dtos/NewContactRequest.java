9e65335871034a4cb12507b84752a195f203a7d8
edurevsky <edurevsky@gmail.com>
2022-08-11 14:22:27 -0300

    add new contact request dto

---
+++ src/main/java/dev/edurevsky/contacts/dtos/NewContactRequest.java
@@ -0,0 +1,19 @@
+package dev.edurevsky.contacts.dtos;
+
+import org.hibernate.validator.constraints.URL;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+public record NewContactRequest(
+    @NotBlank(message = "O Nome não pode estar em branco.")
+    String name,
+    @NotBlank(message = "O Email não pode estar em branco.")
+    @Email(message = "É necessário inserir um email válido")
+    String email,
+    @NotBlank(message = "O Número não pode estar em branco.")
+    String number,
+    @NotBlank(message = "A url da imagem é obrigatória.")
+    @URL(message = "É necessário inserir uma url válida.")
+    String pictureUrl
+) { }
