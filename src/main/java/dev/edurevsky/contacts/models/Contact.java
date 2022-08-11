d9e7273a0fc2ca1d2f6c6e283df188f84270777c
edurevsky <edurevsky@gmail.com>
2022-08-11 14:02:20 -0300

    add contact orm

---
+++ src/main/java/dev/edurevsky/contacts/models/Contact.java
@@ -0,0 +1,66 @@
+package dev.edurevsky.contacts.models;
+
+import javax.persistence.*;
+
+import static javax.persistence.GenerationType.*;
+
+@Entity
+@Table(name = "contacts")
+public class Contact {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "number", nullable = false)
+    private String number;
+
+    @Column(name = "pic_url", nullable = false)
+    private String pictureUrl;
+
+    public Contact() {
+
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getPictureUrl() {
+        return pictureUrl;
+    }
+}
