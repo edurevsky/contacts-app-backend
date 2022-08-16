d9c7b7167e700076d97facaf3c791dc55f829a53
edurevsky <edurevsky@gmail.com>
2022-08-16 10:41:21 -0300

    update security config

--- src/main/java/dev/edurevsky/contacts/configuration/SecurityConfiguration.java
+++ src/main/java/dev/edurevsky/contacts/configuration/SecurityConfiguration.java
@@ -1,15 +1,30 @@
 package dev.edurevsky.contacts.configuration;
 
+import dev.edurevsky.contacts.filters.JWTAuthenticationFilter;
+import dev.edurevsky.contacts.filters.JWTLoginFilter;
+import dev.edurevsky.contacts.utils.JWTUtils;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class SecurityConfiguration {
+
+    private final JWTUtils jwtUtils;
+    private final AuthenticationConfiguration authenticationConfiguration;
+
+    public SecurityConfiguration(JWTUtils jwtUtils, AuthenticationConfiguration authenticationConfiguration) {
+        this.jwtUtils = jwtUtils;
+        this.authenticationConfiguration = authenticationConfiguration;
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -17,11 +32,19 @@ public class SecurityConfiguration {
                 .csrf().disable()
                 .authorizeHttpRequests()
                 .anyRequest()
-                .permitAll()
+                .authenticated()
                 .and()
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http
+                .addFilterBefore(new JWTLoginFilter(jwtUtils, authenticationConfiguration.getAuthenticationManager()), UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(new JWTAuthenticationFilter(jwtUtils), UsernamePasswordAuthenticationFilter.class);
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
     @Bean
