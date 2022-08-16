da6f4e118b0c28a178d02c7efa07275aec0f95d1
edurevsky <edurevsky@gmail.com>
2022-08-16 10:37:43 -0300

    add jwt authentication filter

---
+++ src/main/java/dev/edurevsky/contacts/filters/JWTAuthenticationFilter.java
@@ -0,0 +1,39 @@
+package dev.edurevsky.contacts.filters;
+
+import dev.edurevsky.contacts.utils.JWTUtils;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class JWTAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JWTUtils jwtUtils;
+
+    public JWTAuthenticationFilter(JWTUtils jwtUtils) {
+        this.jwtUtils = jwtUtils;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String token = request.getHeader("Authorization");
+        String jwt = getJwt(token);
+        if (jwtUtils.isValid(jwt)) {
+            UsernamePasswordAuthenticationToken authentication = jwtUtils.getAuthentication(jwt);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private String getJwt(String token) {
+        if (token.startsWith("Bearer ")) {
+            return token.substring(7, token.length());
+        }
+        return null;
+    }
+}
