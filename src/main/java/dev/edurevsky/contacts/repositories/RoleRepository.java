910e328a0fd7433fc19509400b9fc27f891cce60
edurevsky <edurevsky@gmail.com>
2022-08-16 10:00:58 -0300

    add role and user repository

---
+++ src/main/java/dev/edurevsky/contacts/repositories/RoleRepository.java
@@ -0,0 +1,9 @@
+package dev.edurevsky.contacts.repositories;
+
+import dev.edurevsky.contacts.models.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
