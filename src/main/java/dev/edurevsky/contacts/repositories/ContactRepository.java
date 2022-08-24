a5b9070a5051575b8aa5d34ff751e62612349208
edurevsky <edurevsky@gmail.com>
2022-08-24 15:01:52 -0300

    update contact repository

--- src/main/java/dev/edurevsky/contacts/repositories/ContactRepository.java
+++ src/main/java/dev/edurevsky/contacts/repositories/ContactRepository.java
@@ -1,9 +1,14 @@
 package dev.edurevsky.contacts.repositories;
 
 import dev.edurevsky.contacts.models.Contact;
+import dev.edurevsky.contacts.models.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ContactRepository extends JpaRepository<Contact, Long> {
+
+    Page<Contact> findAllByUser(Pageable pageable, User user);
 }
